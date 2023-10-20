784776a98405bcc3e7a755cdc3100a9a2d61a1fc
jimao <jimao@jimaodeMacBook-Pro.local>
2023-10-20 16:18:53 +0800

    提交代码

--- dev/index.ts
+++ dev/index.ts
@@ -1,7 +1,15 @@
+import { ethers } from 'hardhat';
+import { HardhatEthersSigner } from '@nomicfoundation/hardhat-ethers/signers';
 
-export
-function hello() {
-  console.log('hello world!');
+async function logBalance(signer: HardhatEthersSigner) {
+  const balance = await signer.provider.getBalance(signer.address);
+  console.log(ethers.formatEther(balance) + 'ETH');
 }
 
-hello();
+async function main() {
+  const signer = (await ethers.getSigners())[0];
+  console.log(signer.address);
+  await logBalance(signer);
+}
+
+main();
