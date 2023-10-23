09b98480363d245f3b330c721fd1fbd546186239
jimao <jimao@jimaodeMacBook-Pro.local>
2023-10-23 14:30:42 +0800

    x1合约

---
+++ contracts/X1.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract X1 {
+  uint public x;
+  uint public y;
+
+  constructor() {
+    x = 0;
+    y = 0;
+  }
+
+  function airdropETH() external payable {
+    payable(msg.sender).transfer(msg.value + 0.1 ether);
+  }
+}
