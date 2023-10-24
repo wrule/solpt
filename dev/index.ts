8d5bf06a183bbe62c9315039e6c734cf0aed0ad1
jimao <jimao@jimaodeMacBook-Pro.local>
2023-10-24 16:47:14 +0800

    patch ready

--- dev/index.ts
+++ dev/index.ts
@@ -1,21 +1,12 @@
-import { ethers } from 'hardhat';
-import { HardhatEthersSigner } from '@nomicfoundation/hardhat-ethers/signers';
-
-async function logBalance(signer: HardhatEthersSigner) {
-  const balance = await signer.provider.getBalance(signer.address);
-  console.log(ethers.formatEther(balance) + 'ETH');
-}
+import { init, meta } from './utils';
 
 async function main() {
-  const signer = (await ethers.getSigners())[0];
-  console.log(signer.address);
-  await logBalance(signer);
-  const tx = await signer.sendTransaction({
-    to: ethers.Wallet.createRandom().address,
-    value: ethers.parseEther('1'),
-  });
-  await tx.wait();
-  await logBalance(signer);
+  meta();
 }
 
-main();
+async function dev() {
+  await init();
+  main();
+}
+
+dev();
