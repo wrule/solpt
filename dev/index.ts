d5354e44d150209491fcbc95ef2ca72db02addfd
jimao <jimao@jimaodeMacBook-Pro.local>
2023-10-24 11:24:54 +0800

    提交代码

--- dev/index.ts
+++ dev/index.ts
@@ -6,16 +6,26 @@ async function logBalance(signer: HardhatEthersSigner) {
   console.log(ethers.formatEther(balance) + 'ETH');
 }
 
+async function deployContract(name: string) {
+  console.log(name, 'deployContract...');
+  let contract = await ethers.deployContract(name);
+  console.log(name, 'waitForDeployment...');
+  contract = await contract.waitForDeployment();
+  console.log(name, 'deployment successful!');
+  console.log(name, 'contract address:', contract.target);
+}
+
 async function main() {
-  const signer = (await ethers.getSigners())[0];
-  console.log(signer.address);
-  await logBalance(signer);
-  const tx = await signer.sendTransaction({
-    to: ethers.Wallet.createRandom().address,
-    value: ethers.parseEther('1'),
-  });
-  await tx.wait();
-  await logBalance(signer);
+  await deployContract('X1');
+  // const signer = (await ethers.getSigners())[0];
+  // console.log(signer.address);
+  // await logBalance(signer);
+  // const tx = await signer.sendTransaction({
+  //   to: ethers.Wallet.createRandom().address,
+  //   value: ethers.parseEther('1'),
+  // });
+  // await tx.wait();
+  // await logBalance(signer);
 }
 
 main();
