de41cdc2957369ee99ef7576cdfe47193803ef9f
jimao <jimao@jimaodeMacBook-Pro.local>
2023-10-24 14:21:09 +0800

    x2合约

---
+++ contracts/X2.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract X2 {
+  event fallbackEvent(address indexed sender, uint value, bytes data);
+  fallback() external payable {
+    emit fallbackEvent(msg.sender, msg.value, msg.data);
+  }
+
+  event receiveEvent(address indexed sender, uint value);
+  receive() external payable {
+    uint i = 0;
+    for (i = 0; i < 100; ++i) {
+      emit receiveEvent(msg.sender, msg.value);
+    }
+  }
+}
