4c4d1053c88e9e8b673c727536b51a60d68cd2a3
jimao <jimao@jimaodeMacBook-Pro.local>
2023-10-24 11:42:06 +0800

    提交代码

--- dev/index.ts
+++ dev/index.ts
@@ -1,21 +1,37 @@
 import { ethers } from 'hardhat';
+import hardhat from 'hardhat';
 import { HardhatEthersSigner } from '@nomicfoundation/hardhat-ethers/signers';
+
+let signer: HardhatEthersSigner;
 
 async function logBalance(signer: HardhatEthersSigner) {
   const balance = await signer.provider.getBalance(signer.address);
   console.log(ethers.formatEther(balance) + 'ETH');
 }
 
+function short_address(address: string) {
+  return address.slice(0, 5) + '...' + address.slice(address.length - 3);
+}
+
+async function meta() {
+  const balance = ethers.formatEther(await signer.provider.getBalance(signer.address));
+  console.log(short_address(signer.address), balance + 'ETH');
+}
+
 async function deployContract(name: string) {
+  console.log('<DeployContract>');
+  meta();
   console.log(name, 'deployContract...');
   let contract = await ethers.deployContract(name);
   console.log(name, 'waitForDeployment...');
   contract = await contract.waitForDeployment();
   console.log(name, 'deployment successful!');
   console.log(name, 'contract address:', contract.target);
+  meta();
 }
 
 async function main() {
+  signer = (await ethers.getSigners())[0];
   await deployContract('X1');
   // const signer = (await ethers.getSigners())[0];
   // console.log(signer.address);
