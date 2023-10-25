b0bf28042f94e42ef31be27be98e7919e0ca47a6
jimao <jimao@jimaodeMacBook-Pro.local>
2023-10-25 16:26:14 +0800

    提交代码

--- dev/index.ts
+++ dev/index.ts
@@ -1,7 +1,22 @@
-import { init, meta } from './utils';
+import { send } from 'process';
+import { X3 } from '../typechain-types';
+import { deployContract, getContract, init, meta } from './utils';
 
 async function main() {
-  meta();
+  await meta();
+  // const x3 = await getContract<X3>('X3', '0x5FbDB2315678afecb367f032d93F642f64180aa3');
+  // await x3.sendMessage('你好，世界');
+
+  const x3 = await deployContract<X3>('X3');
+  x3.on(x3.getEvent('sendMessageEvent'), (message: string) => {
+    console.log('sendMessageEvent', message);
+  });
+  x3.on(x3.getEvent('receiveEvent'), (sender: string, value: bigint) => {
+    console.log('receiveEvent', sender, value);
+  });
+  x3.on(x3.getEvent('fallbackEvent'), (sender: string, value: bigint, data: string) => {
+    console.log('fallbackEvent', sender, value, data);
+  });
 }
 
 async function dev() {
