60e96d5e0c18338d03fe06cdfd609e5fb74f1738
jimao <jimao@jimaodeMacBook-Pro.local>
2023-10-26 12:51:31 +0800

    提交代码

--- contracts/X4.sol
+++ contracts/X4.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.20;
 contract X4 {
   string[] public list;
 
-  event sendMessageEvent(string message);
+  event sendMessageEvent(string message, bytes data, bytes4 sig);
   function sendMessage(string calldata message) public {
     list.push(message);
-    emit sendMessageEvent(message);
+    emit sendMessageEvent(message, msg.data, msg.sig);
   }
 }
--- dev/index.ts
+++ dev/index.ts
@@ -1,7 +1,15 @@
-import { init, meta } from './utils';
+import { X4 } from '../typechain-types';
+import { deployContract, init, meta } from './utils';
 
 async function main() {
   await meta();
+  const x4 = await deployContract<X4>('X4');
+  x4.on(x4.getEvent('sendMessageEvent'), (message) => {
+    console.log(message);
+  });
+  await x4.sendMessage('你好啊');
+  const text = await x4.list(0);
+  console.log(text);
 }
 
 async function dev() {
