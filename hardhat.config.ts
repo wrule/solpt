dca37f98d63ed46cc1253ac872456fe6ceadc94c
jimao <jimao@jimaodeMacBook-Pro.local>
2023-10-27 17:35:22 +0800

    提交代码

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-verify";
+
+const secret = require('./.secret');
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
+  networks: {
+    sepolia: {
+      url: secret.alchemy.url,
+      accounts: secret.privateKey,
+    },
+  },
+  sourcify: {
+    enabled: true,
+  },
+  etherscan: secret.etherscan,
 };
 
 export default config;
