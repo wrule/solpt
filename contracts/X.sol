6519d8a818af1e99c449127ea00dd62c6d588d2b
jimao <jimao@jimaodeMacBook-Pro.local>
2023-10-27 18:25:17 +0800

    提交代码

--- contracts/X.sol
+++ contracts/X.sol
@@ -1,10 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+
 contract X {
-  event sendMessageEvent(string message);
+  event sendMessageEvent(address message);
   function sendMessage(string memory message) public {
-    emit sendMessageEvent(message);
+    // emit sendMessageEvent(keccak256("ABCD"));
+    emit sendMessageEvent(ECDSA.recover(
+      hex"380760bcae8fa72cda68cca2399a5714d8acecbe3e75054bbc02884fc9837f3b",
+      hex"79213aacec369db9de6f458813cb55b9f267432ad0f629d713376df35a38407f75a37625a30ee5c4346ef1e445abc39fdeee716add3d56fe5d0b620a22ecaf181c"
+    ));
   }
 
   event fallbackEvent(address sender, uint value, bytes data);
--- dev/index.ts
+++ dev/index.ts
@@ -1,7 +1,23 @@
-import { init, meta } from './utils';
+import { ethers } from 'hardhat';
+import { deployContract, getSigner, init, meta, watchContract } from './utils';
+import { X } from '../typechain-types';
 
 async function main() {
   await meta();
+  // const x = await deployContract<X>('X');
+  // watchContract(x);
+  // await x.sendMessage('ABCD');
+  const data = ethers.toUtf8Bytes('ABCD');
+  const a = await getSigner().signMessage(data);
+  console.log(a);
+  const b = ethers.verifyMessage(data, a);
+  console.log(b);
+  // const a = ethers.keccak256(ethers.toUtf8Bytes(text));
+  // console.log(a);
+  // console.log(Buffer.from(a.replace('0x', ''), 'hex'));
+  // const b = await getSigner().signMessage(Buffer.from(a.replace('0x', ''), 'hex'));
+  // // console.log(a);
+  // console.log(b);
 }
 
 async function dev() {
