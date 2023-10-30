e66d5caedc05fc4018424ed3827ad8c6d7676941
jimao <jimao@jimaodeMacBook-Pro.local>
2023-10-30 13:44:20 +0800

    提交代码

--- dev/index.ts
+++ dev/index.ts
@@ -3,16 +3,19 @@ import { deployContract, getSigner, init, meta, watchContract } from './utils';
 import { X } from '../typechain-types';
 
 async function main() {
-  // const x = await deployContract<X>('X');
-  const wallet = new ethers.Wallet('0x227dbb8586117d55284e26620bc76534dfbd2394be34cf4a09cb775d593b6f2b');
-  console.log(wallet.address);
+  const x = await deployContract<X>('X');
+  // const wallet = new ethers.Wallet('0x227dbb8586117d55284e26620bc76534dfbd2394be34cf4a09cb775d593b6f2b');
+  // console.log(wallet.address);
   const a = ethers.solidityPackedKeccak256(['address', 'uint256'], ['0x5B38Da6a701c568545dCfcB03FcB875f56beddC4', 0]);
   console.log(a);
   const b = ethers.solidityPackedKeccak256(['string', 'bytes32'], ['\x19Ethereum Signed Message:\n32', a]);
   console.log(b);
   const buffer = Buffer.from(a.replace('0x', ''), 'hex');
-  const c = await wallet.signMessage(buffer);
+  const c = await getSigner().signMessage(buffer);
   console.log(c);
+
+  const d = await x.ecd(b, c);
+  console.log(d);
   // await meta();
 
   // const a = ethers.solidityPackedKeccak256(['address', 'uint256'], ['0x5B38Da6a701c568545dCfcB03FcB875f56beddC4', 0]);
