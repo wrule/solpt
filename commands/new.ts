f9378e564e00636e6df039c5e3e2f63155c3382e
jimao <jimao@jimaodeMacBook-Pro.local>
2023-10-31 10:27:30 +0800

    solpt

--- commands/new.ts
+++ commands/new.ts
@@ -9,15 +9,15 @@ function replace(filepath: string, search: any, replace: string) {
   );
 }
 
-if (!fs.readFileSync('package.json', 'utf8').includes('nodept')) {
+if (!fs.readFileSync('package.json', 'utf8').includes('solpt')) {
   console.log('project has been created');
   process.exit(0);
 }
 const name = process.argv[process.argv.length - 1];
 console.log(name);
-replace('package.json', /nodept/g, name);
+replace('package.json', /solpt/g, name);
 replace('package.json', /"version": "(\d+|\.)+"/, '"version": "0.1.0"');
-replace('package-lock.json', /nodept/g, name);
-replace('README.md', /nodept/g, name);
+replace('package-lock.json', /solpt/g, name);
+replace('README.md', /solpt/g, name);
 child_process.execSync('npm install');
 child_process.execSync(`rm -rf .git && git init && git add . && git commit -m 'init'`);
