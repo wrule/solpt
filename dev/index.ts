c3c203d83d2ac5b2f2e167ed750b1005ecdae47e
jimao <jimao@jimaodeMacBook-Pro.local>
2023-11-07 14:04:20 +0800

    npm的验证命令

--- dev/index.ts
+++ dev/index.ts
@@ -1,7 +1,9 @@
-import { init, meta } from './utils';
+import { X } from '../typechain-types';
+import { deployContract, init, meta } from './utils';
 
 async function main() {
   await meta();
+  const x = await deployContract<X>('X');
 }
 
 async function dev() {
