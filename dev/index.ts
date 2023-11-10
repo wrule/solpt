ca2bcec5b7c104ab68e584304876a7a703dc9b72
jimao <jimao@jimaodeMacBook-Pro.local>
2023-11-10 15:24:40 +0800

    类型

--- dev/index.ts
+++ dev/index.ts
@@ -6,6 +6,9 @@ import * as A from '../typechain-types/index';
 async function main() {
   await meta();
   type TA = ReturnType<typeof A.factories.A__factory.connect>;
+  type B = typeof A.factories;
+  type C = B["A__factory"]["connect"];
+  type D = ReturnType<C>;
 }
 
 async function dev() {
