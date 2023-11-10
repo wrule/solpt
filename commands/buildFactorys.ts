4c90dd6aa95ddb8ce1417a534df7a13e157a8bd0
jimao <jimao@jimaodeMacBook-Pro.local>
2023-11-10 16:22:44 +0800

    buildFactorys

---
+++ commands/buildFactorys.ts
@@ -0,0 +1,20 @@
+import fs from 'fs';
+import * as Types from '../typechain-types';
+
+function contractNames() {
+  return Object.keys(Types.factories)
+    .map((name) => /^(.+)__factory$/.exec(name)?.[1])
+    .filter((name) => name);
+}
+
+function main() {
+  fs.writeFileSync('typechain-types/factorys.ts', `
+import * as Types from '.';
+
+export default {
+${contractNames().map((name) => `  '${name}': Types.${name}__factory,`).join('\n')}
+};
+`.trim() + '\n');
+}
+
+main();
